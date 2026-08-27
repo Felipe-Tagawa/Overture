455321fbabc47d5ef6f2c7010cf3e72094bd71a7
Felipe Tagawa <felipereisbkp@gmail.com>
2026-08-27 02:39:22 -0300

    fix: remove useless files

--- data/test.py
+++
@@ -1,9 +0,0 @@
-from api_client import get_pokemon_detail, get_pokemon_list
-
-lista = get_pokemon_list(limit=5)
-print(lista)
-
-print("*"*5)
-
-pikachu = get_pokemon_detail("pikachu")
-print(pikachu)
