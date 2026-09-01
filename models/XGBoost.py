369db40697f9de5e80f26db0a8ba00118774e77e
Felipe Tagawa <felipereisbkp@gmail.com>
2026-09-01 01:56:23 -0300

    fix: benchmark.py foi levado para runners/

--- models/XGBoost.py
+++ models/XGBoost.py
@@ -25,35 +25,50 @@ X_train, X_test, y_train, y_test, y_orig_train, y_orig_test = train_test_split(
 # Ele é mais rápido e eficiente para datasets grandes, mas não suporta todas as funcionalidades do GradientBoostingRegressor.
 
 # https://xgboost.readthedocs.io/en/stable/python/python_api.html#xgboost.XGBRegressor
-model = XGBRegressor(
-    n_estimators=100,
-    learning_rate=0.1,
-    max_depth=6,
-    sub_sample=0.8,
-    min_sample_split=10,    
-    early_stopping_rounds=10, 
-    eval_metric="rmse",
-    # reg_alpha=0.1, estudar se vale a pena usar a regularização L1 ou L2 aqui
-    verbosity=3,
-    random_state=42
-)
 
-model.fit(X_train, y_train, eval_set=[(X_test, y_test)], verbose=True)
+def make_model_xgb(
+        n_jobs=8,
+        verbosity=0,
+        n_estimators=50,
+        learning_rate=0.1,
+        max_depth=6,
+        subsample=0.8,
+        min_child_weight=10,
+) -> XGBRegressor:
+    return XGBRegressor(
+        n_estimators=n_estimators,
+            learning_rate=learning_rate,
+            max_depth=max_depth,
+            subsample=subsample,
+            min_child_weight=min_child_weight,    
+            early_stopping_rounds=10, 
+            eval_metric="rmse",
+            n_jobs=n_jobs,
+            # reg_alpha=0.1, estudar se vale a pena usar a regularização L1 ou L2 aqui
+            verbosity=verbosity,
+            random_state=42
+    )
 
-y_pred_log = model.predict(X_test)
-y_pred = np.expm1(y_pred_log) # Reverter log1p
+model_xgb = make_model_xgb(verbosity=3)
 
-mae = mean_absolute_error(y_orig_test, y_pred)
-mse = mean_squared_error(y_orig_test, y_pred)
-r2 = r2_score(y_orig_test, y_pred)
+if __name__ == "__main__":
 
-print(f"MAE: {mae:.5f}")
-print(f"RMSE: {mse:.5f}")
-print(f"R²: {r2:.5f}")
+    model_xgb.fit(X_train, y_train, eval_set=[(X_test, y_test)], verbose=True)
 
-importances = dict(zip(FEATURES, model.feature_importances_)) # Criar dicionário de importância
-print("\nImportância das features:")
+    y_pred_log = model_xgb.predict(X_test)
+    y_pred = np.expm1(y_pred_log) # Reverter log1p
 
-# '-' para ser decrescente e '[1]' para pegar o valor e não a chave
-for feat, importance in sorted(importances.items(),  key=lambda x: -x[1]):
-    print(f"{feat}: {importance:.4f}")
+    mae = mean_absolute_error(y_orig_test, y_pred)
+    rmse = np.sqrt(mean_squared_error(y_orig_test, y_pred))
+    r2 = r2_score(y_orig_test, y_pred)
+
+    print(f"MAE: {mae:.5f}")
+    print(f"RMSE: {rmse:.5f}")
+    print(f"R²: {r2:.5f}")
+
+    importances = dict(zip(FEATURES, model_xgb.feature_importances_)) # Criar dicionário de importância
+    print("\nImportância das features:")
+
+    # '-' para ser decrescente e '[1]' para pegar o valor e não a chave
+    for feat, importance in sorted(importances.items(),  key=lambda x: -x[1]):
+        print(f"{feat}: {importance:.4f}")
